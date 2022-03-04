314a531b8ecac41d0dfed6a805af7b4bd8f72eaa
Tsubasa Kondo <kondows95@gmail.com>
2022-03-04 21:53:12 +0900

    相関係数を調べる機能を追加

--- invest-simulator.js
+++ invest-simulator.js
@@ -1,40 +1,79 @@
 //固定パラメータ
-const NUM_LOOP = 1;//シミュレーション施行回数
-const YEARS = 1/3;
+const NUM_LOOP = 1000;//シミュレーション施行回数
 const INCOME = 30;//入金額
+const YEARS = 1/3;
 const PERIOD = 360 * YEARS;
 const NUM_PERIODS = 3;//上下上など3段階で値が大きく動く
-const REBALANCE_INTERVAL = 30;//リバランスを実施する間隔(単位は日)
-const DO_REBALANCE = true;//リバランスはやらない方が良い
+const REBALANCE_INTERVAL = 365;//リバランスを実施する間隔(単位は日)
+const DO_REBALANCE = true;//リバランスするか？
 const ICASH = 0;//assets[0]は必ず現金にする。
 const FIRST_CHART_PRICE = 1;//変更不可!
+
+
+const createPurchaseSam = () => {
+    return {
+        sellValues: 0,//sellの評価額(単価*口数)
+        sellNums: 0,//sellの口数
+        buyValues: 0,//buyの評価額(単価*口数)
+        buyNums: 0,//buyの口数
+    };
+}
+let PURCHASE_SUM = createPurchaseSam();
 
 /**
  * シミュレーション用の株価を取得。
  * 
  * @param  {number} keikiHosei 騰落率の補正値(例不景気なら-0.1)
+ * @param  {number} hoseiMagnification 補正倍率(逆相関にしたければマイナス値を指定)
  * @param  {number} upRate 騰落率の最大値(例:5%なら5)
- * @param  {number} numTry 平均値を取る際の試行回数(少ないほど乱高下)
- * @param  {Boolean} positive 景気補正に相関するか(逆相関ならfalse)
+ * @param  {number} avgDenominator 平均値を取る際の試行回数(少ないほど乱高下)
+ 
  * @return {number} 騰落率(例:-4%なら-4)
  */
-const getUpdownRate = (keikiHosei, updown=5, numTry=10, positive=true) => {
-    //updownが多いほうが景気の影響を受けやすく、ボラティリティが高くなるようにする。
-    //positiveがfalseの場合は補正値の符号を反転させて景気と逆相関にする。
-    let hosei = keikiHosei * updown;
-    if (!positive) {
-        hosei = -hosei;
-    }
+const getUpdownRate = (keikiHosei, hoseiMagnification, updown, avgDenominator) => {
+    //hoseiMagnificationが多いと景気の影響を強く受ける(マイナスだと逆相関)
+    //updownが多いとボラティリティが高い。
+    const hosei = keikiHosei * hoseiMagnification;
     const max = updown + hosei;
     const min = -updown + hosei;  
     
     //複数回試行して平均を取ることで値動きをマイルドにする。
-    numTry = Math.max(1, numTry);
+    avgDenominator = Math.max(1, avgDenominator);
     let sum = 0;
-    for (let i=0; i<numTry; i++) {
+    for (let i=0; i<avgDenominator; i++) {
         sum += Math.random() * (max - min) + min;
     }
-    return (sum / numTry);
+    return (sum / avgDenominator);
+}
+/**
+ * 相関係数を取得。
+ * 
+ * @param  {Array} xx 配列1
+ * @param  {Array} yy 配列2
+ * @return {number} 相関係数
+ */
+const sampleCorrelation = (xx, yy) => {
+    let m = Math, n;
+    if (xx.length == yy.length) {
+        n = xx.length;
+        let sumx=0, sumy=0, xm ,ym ,xxi ,yyi,sumxxm=0, sumyym=0, sumxym=0
+        for(let i=0; i<n; i++){
+            sumx += (xx[i]-0);
+            sumy += (yy[i]-0);
+        }
+        xm = sumx/n;
+        ym = sumy/n;
+        for(let i=0; i<n; i++){
+            xxi = (xx[i]-0);
+            yyi = (yy[i]-0);
+            sumxxm += (xxi-xm) * (xxi-xm);
+            sumyym += (yyi-ym) * (yyi-ym);
+            sumxym += (xxi-xm) * (yyi-ym);
+        }
+        return sumxym / m.sqrt(sumxxm) / m.sqrt(sumyym);
+    } else {
+        throw new Error("Array length is not same.");
+    }
 }
 
 /**
@@ -63,7 +102,7 @@ const divideAssets = (assets, dealAmount) => {
 }
 
 /**
- * 時価総額と比率に応じて資産を分割する。
+ * 時価総額ベースで資産を分割する。
  * 
  * @param  {Array} assets assetオブジェクトの配列
  * @return {Array} 新しい配分の配列
@@ -90,6 +129,18 @@ const divideAssets = (assets, dealAmount) => {
 }
 
 /**
+ * 桁数指定で四捨五入
+ * 
+ * @param  {Number} num 数値
+ * @param  {Number} digit 桁数
+ * @return {Number} 四捨五入した数
+ */
+const round = (num, digit=1) => {
+    const digitVal = Math.pow(10, digit);
+    return Math.round(num * digitVal) / digitVal;
+}
+
+/**
  * 資産配分を実行する。
  * 
  * @param  {Array} assets assetオブジェクトの配列
@@ -103,16 +154,19 @@ const rebalance = (assets) => {
     const history = [];
     if (balances[ICASH] >= 0) {
         balances.forEach((bal, i) => {
-            const diff = Math.round(bal - assets[i].amount);
+            const diff = bal - assets[i].amount;//デバッグ用
             assets[i].amount = bal;
-            //デバッグ用
-            const value = Math.round(assets[i].lastChartPrice *　assets[i].amount);
-            const lastChartPrice = Math.round(assets[i].lastChartPrice);
-            const amount = Math.round(assets[i].amount);
-            history.push(`${diff>0?'+':''}${diff}|${lastChartPrice}*${amount}=${value}`);
+            //確認用
+            if (diff > 0) {
+                PURCHASE_SUM.buyValues += diff * assets[i].lastChartPrice;//口数*時価
+                PURCHASE_SUM.buyNums += diff;//口数
+            } else {
+                PURCHASE_SUM.sellValues -= diff * assets[i].lastChartPrice;//口数*時価
+                PURCHASE_SUM.sellNums -= diff;//口数
+            }
+            history.push(`${diff>0?'+':''}${round(diff)}|${round(assets[i].lastChartPrice)}*${round(assets[i].amount)}=${round(assets[i].lastChartPrice *　assets[i].amount)}`);
         });
-    }
-    else {
+    } else {
         console.log('現金不足でリバランスできません！')
     }
     return history;
@@ -126,29 +180,32 @@ const rebalance = (assets) => {
  * @param  {String} label assetオブジェクトの配列
  * @param  {Number} balance 資産配分の比率(9割なら9)
  * @param  {Number} updown その資産の日毎の騰落率の最大値(-5%なら-5)
- * @param  {Number} volatility 逆ボラティリティ(低いほど乱高下)
- * @param  {Boolean} positive 値動きに相関するか(逆相関ならfalse)
+ * @param  {Number} hoseiMagnification 補正倍率(景気と逆相関にするならマイナス値を指定)
+ * @param  {Boolean} avgDenominator 多いほど値動きがマイルドになる
  * @return {Object} Assetオブジェクト
  */
-const createAsset = (label, balance, updown=0, volatility=10, positive=true) => {
+const createAsset = (label, balance, updown=0, hoseiMagnification=10, avgDenominator=true) => {
     return {
         label: label,
         updown: updown,
-        volatility: volatility,
-        positive: positive,
+        hoseiMagnification: hoseiMagnification,
+        avgDenominator: avgDenominator,
         balance: balance,
-        amount: 0,
+        amount: 0,//口数(評価額でないことに注意!)
         firstChartPrice: FIRST_CHART_PRICE,
         lastChartPrice: FIRST_CHART_PRICE,
+        chartPriceLogs: [],
     };
+    //e(keikiHosei, asset.hoseiMagnification, asset.updown, asset.avgDenominator) 
 }
 
-const runShortSimulation = (days, assets, interval, keikiHosei) => {
-    for (let day=0; day<days; day++) {
-        //各々の騰落率に応じて株価を増減させる。
+const runShortSimulation = (assets, keikiHosei) => {
+    for (let day=0; day<PERIOD; day++) {
+        //各々の騰落率に応じて株価を毎日増減させる。
         assets.forEach((asset, i) => {
-            const rate = getUpdownRate(keikiHosei, asset.updown, asset.volatility, asset.positive) / 100 + 1;
+            const rate = getUpdownRate(keikiHosei, asset.hoseiMagnification, asset.updown, asset.avgDenominator) / 100 + 1;
             assets[i].lastChartPrice = assets[i].lastChartPrice * rate;
+            assets[i].chartPriceLogs.push(assets[i].lastChartPrice);
         });
         assets[ICASH].lastChartPrice = FIRST_CHART_PRICE;//冗長だが一応
 
@@ -160,18 +217,23 @@ const runShortSimulation = (days, assets, interval, keikiHosei) => {
             //時価総額に応じた口数に変更する。
             const nums = divs.map((div, i) => div / assets[i].lastChartPrice);
 
+            //確認用
+            nums.forEach((num, i) => {
+                PURCHASE_SUM.buyValues += divs[i];//口数*時価
+                PURCHASE_SUM.buyNums += num;//口数
+            });
+
             //assetsに購入した口数を反映する。
             nums.forEach((num, i) => {
                 assets[i].amount += num
             });
             if (NUM_LOOP === 1) {
-                console.log(nums)
-                //console.log(nums.map((num, i) => num));
+                console.log(nums.map((n) => round(n)));
             }
         }
 
         //一定期間毎にリバランスを実行。
-        if (day !== 0 && (day % interval === 0 || day === days-1)) {
+        if (day !== 0 && (day % REBALANCE_INTERVAL === 0 || day === PERIOD-1)) {
             if (DO_REBALANCE) {
                 const history = rebalance(assets);
                 if (NUM_LOOP === 1) {
@@ -182,22 +244,23 @@ const runShortSimulation = (days, assets, interval, keikiHosei) => {
     }
 }
 
-const getBalanceLabel = (balances) => {
-    return balances.reduce((str, bal) => str + ":" + bal);
-}
-
 const getTotalIncome = () => {
     return (INCOME * PERIOD/30) * NUM_PERIODS;
 }
 
-const createSummary = (strLabel, strBalance) => {
+const createSummary = (strLabel, balances) => {
+    const assetLength = createAssets().length;
     const summary = {
         label: strLabel,
-        balance: strBalance,
-        total: 0,
-        lastChartPrices: Array(createAssets().length),
+        balance: balances.reduce((str, bal) => str+'|'+bal),
+        totalValue: 0,//時価総額
+        lastChartPrices: initArray(assetLength),
+        chartPriceLogs: Array(assetLength),
     };
-    summary.lastChartPrices.fill(0);
+    for (let i=0; i<assetLength; i++) {
+        //資産毎の全期間分の日毎の株価のチャートを初期化。
+        summary.chartPriceLogs[i] = initArray(PERIOD * NUM_PERIODS);
+    }
     return summary;
 }
 
@@ -206,11 +269,14 @@ const runMultipulSimulation = (economyChart, balances) => {
     for (let num=0; num<NUM_LOOP; num++) { 
         const assets = createAssets(balances);
         economyChart.V.forEach((keikiHosei) => {
-            runShortSimulation(PERIOD, assets, REBALANCE_INTERVAL, keikiHosei);
+            runShortSimulation(assets, keikiHosei);
         });
         assets.forEach((asset, i) => {
-            summary.total += asset.amount;
+            summary.totalValue += asset.amount * asset.lastChartPrice;
             summary.lastChartPrices[i] += asset.lastChartPrice;
+            asset.chartPriceLogs.forEach((price, j) => {
+                summary.chartPriceLogs[i][j] += price;
+            });
         });
     }
     return summary;
@@ -219,10 +285,9 @@ const runMultipulSimulation = (economyChart, balances) => {
 const createAssets = (balances=null) => {
     //必ずassets[ICASH]が現金になるようにしてください。
     const assets = [
-        createAsset('現金', 0, 0, 1, false),//変更禁止(ICASH)
-        createAsset('株式', 0, 10, 100, true),
-        //createAsset('逆相関株式', 0, 7, 10, false),
-        createAsset('債権', 0, 10, 100, true),
+        createAsset('現金', 0, 0, 0, 1),//削除・順番変更禁止!!
+        createAsset('株式', 0, 7, 7, 5),
+        createAsset('債券', 0, 2, 0.05, 5),
     ];
     if (Array.isArray(balances)) {
         balances.forEach((bal, i) => {
@@ -234,27 +299,115 @@ const createAssets = (balances=null) => {
 
 const printSummary = (summary, numCharts=1) => {
     const div = NUM_LOOP*numCharts;
-    const avgTotal = Math.round(summary.total/div);
+    const avgTotal = Math.round(summary.totalValue/div);
     const growthRates = summary.lastChartPrices.map((price) => {
         return Math.round(getGrowthRate(price/div, FIRST_CHART_PRICE)) + '%';
     });
-    const strGrowthRates = growthRates.reduce((str, price) => str + '/' + price);
+    const strGrowthRates = growthRates.reduce((str, price) => str + '|' + price);
     const interest = getGrowthRate(avgTotal, getTotalIncome()) + '%';
     console.log(`${summary.label}\t${summary.balance}\t ${avgTotal}\t${interest}\t ${strGrowthRates}`)
+    
+    if (summary.label !== '期待値') {
+        console.log('相関係数', sampleCorrelation(summary.chartPriceLogs[1], summary.chartPriceLogs[2]));
+    }
 }
 
 const printTitle = () => {
     console.log('値動き  資産配分 総資産 利回り   騰落率');
 }
 
+const initArray = (length, value=0) => {
+    const arr = Array(length);
+    arr.fill(value);
+    return arr;
+}
+
+/**
+ * ２つの資産の相関係数を調べる。
+ * おもにcreateAssetのパラメータ値を決定する為に使用する。
+ */
+const testCorrelation = () => {
+    const numLoop = 1000;
+    const x = +0.008;
+    const chart = [x,x,x];
+    const periodDays = 365;
+    let sumCorrel = 0;
+    let sumLastPrice1 = 0;
+    let sumLastPrice2 = 0;
+    for (let iMulti=0; iMulti<numLoop; iMulti++) {
+        //label,balance,updown,hoseiMagnification,avgDenominator
+        const asset1 = createAsset('株式', 0, 7, 7, 5);
+        const asset2 = createAsset('債券', 0, 2, 3, 5);
+        const prices1 = initArray(periodDays * chart.length);
+        const prices2 = initArray(periodDays * chart.length);
+        let day = 0;
+        chart.forEach((keikiHosei) => {
+            for (let iPeriod=0; iPeriod<periodDays; iPeriod++) {
+                const rate1 = getUpdownRate(keikiHosei, asset1.hoseiMagnification, asset1.updown, asset1.avgDenominator) / 100 + 1;
+                const rate2 = getUpdownRate(keikiHosei, asset2.hoseiMagnification, asset2.updown, asset2.avgDenominator) / 100 + 1;
+                asset1.lastChartPrice = asset1.lastChartPrice * rate1;
+                asset2.lastChartPrice = asset2.lastChartPrice * rate2;
+                prices1[day] = asset1.lastChartPrice;
+                prices2[day] = asset2.lastChartPrice;
+                day++;
+            }
+        });
+        sumCorrel += sampleCorrelation(prices1, prices2);
+        sumLastPrice1 += asset1.lastChartPrice;
+        sumLastPrice2 += asset2.lastChartPrice;
+    }
+    console.log('相関係数', sumCorrel/numLoop);
+    console.log('騰落率1', getGrowthRate(sumLastPrice1/numLoop, 1)+'%');
+    console.log('騰落率2', getGrowthRate(sumLastPrice2/numLoop, 1)+'%');
+}
+
 const runSimulation = () => {
-    const DEBUGHOSEI = 0.01
+    //景気チャート
+    const economyCharts = createEconomyCharts();
+
+    //資産配分のパターン
+    const balancePattern = [
+        [0,10,0],
+        [0,5,5],
+        [0,0,10],
+    ];
+
+    const strReb = DO_REBALANCE ? 'リバランス頻度'+REBALANCE_INTERVAL+'日' : 'リバランス無し';
+    console.log(`期間${YEARS*3}年：入金額${getTotalIncome()}円(月${INCOME}円)：${strReb}：試行${NUM_LOOP}回`);
+
+    TEST = initArray(balancePattern[0].length);
+
+    //全体の処理を行うループ処理。
+    balancePattern.forEach((balances, i) => {
+        if (i ===0 || true) {
+            printTitle();
+        }
+        PURCHASE_SUM = createPurchaseSam();
+        const allSummary = createSummary("期待値", balances);
+        economyCharts.forEach((economyChart) => {
+            const subSummary = runMultipulSimulation(economyChart, balances);
+            allSummary.totalValue += subSummary.totalValue;
+            subSummary.lastChartPrices.forEach((price, i) => {
+                allSummary.lastChartPrices[i] += price;
+            });
+            printSummary(subSummary);
+        });
+        printSummary(allSummary, economyCharts.length);
+        const div = NUM_LOOP*economyCharts.length;
+        if (PURCHASE_SUM.sellNums > 0) {
+            console.log('売却単価:'+round(PURCHASE_SUM.sellValues/PURCHASE_SUM.sellNums,3)+'円('+round(PURCHASE_SUM.sellNums/div)+')');
+        }
+        console.log('購入単価:'+round(PURCHASE_SUM.buyValues/PURCHASE_SUM.buyNums,3)+'円('+round(PURCHASE_SUM.buyNums/div,0)+')');
+    });   
+}
+
+const createEconomyCharts = () => {
     //景気チャート
     const P = +0.008 ;
     const M = -0.008;
-    const X = -0.01;
+    const X = +0.01;
     const economyCharts = [
-        {K:"TEST", V:[X,X,X]},
+        {K:"TEST", V:[P,P,P]},
         // {K:"下上上", V:[M,P,P]},
         // {K:"上下上", V:[P,M,P]},
         // {K:"上上下", V:[P,P,M]},
@@ -262,30 +415,9 @@ const runSimulation = () => {
         // {K:"下上下", V:[M,P,M]},
         // {K:"上下下", V:[P,M,M]},
     ];
-
-    //資産配分のパターン
-    const balancePattern = [
-        [0,5,5],
-    ];
-
-    console.log(`期間${YEARS*3}年：入金額${getTotalIncome()}円(月${INCOME}円)：リバランス頻度${REBALANCE_INTERVAL}日：試行${NUM_LOOP}回：`)
-
-    //全体の処理を行うループ処理。
-    balancePattern.forEach((balances, i) => {
-        if (i ===0 || true) {
-            printTitle();
-        }
-        const allSummary = createSummary("期待値", balances);
-        economyCharts.forEach((economyChart) => {
-            const subSummary = runMultipulSimulation(economyChart, balances);
-            allSummary.total += subSummary.total;
-            subSummary.lastChartPrices.forEach((price, i) => {
-                allSummary.lastChartPrices[i] += price;
-            });
-            printSummary(subSummary);
-        });
-        printSummary(allSummary, economyCharts.length);
-    });
+    return economyCharts;
 }
-runSimulation();
+
+//runSimulation();
+testCorrelation();
 
